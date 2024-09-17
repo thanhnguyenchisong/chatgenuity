677e41b1370e05ba0ba29cad53f3648f2f37e431
vanduc2514 <vanduc2514@gmail.com>
2024-09-17 19:54:21 +0000

    please don't hard code the env variable

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const API_HOST = import.meta.env.VITE_API_HOST || 'http://localhost:8080';
-const INTERVIEW_HOST = import.meta.env.VITE_INTERVIEW_HOST || 'http://localhost:8800';
-const KL_HOST = import.meta.env.VITE_KL_HOST || 'http://localhost:9000';
-const KL_REALM= import.meta.env.VITE_KL_REALM || 'chatbotdev';
-const KL_CLIENT= import.meta.env.VITE_KL_CLIENT || 'chatbotclient';
+const API_HOST = import.meta.env.VITE_API_HOST || '';
+const INTERVIEW_HOST = import.meta.env.VITE_INTERVIEW_HOST || '';
+const KL_HOST = import.meta.env.VITE_KL_HOST || '';
+const KL_REALM= import.meta.env.VITE_KL_REALM || '';
+const KL_CLIENT= import.meta.env.VITE_KL_CLIENT || '';
 export { API_HOST, INTERVIEW_HOST, KL_HOST, KL_REALM, KL_CLIENT };
